593af0457f019549db908e65ee587e81cd1fc8af
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2018-02-04 11:19:22 -0500

    oof

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ purpose of the file is to pass control to the app’s first module.
 
 var application = require("application");
 
-application.start({ moduleName: "home/home-page" });
+application.start({ moduleName: "home/slides" });
 
 /*
 Do not place any code after the application has been started as it will not
--- app/home/slides.js
+++ app/home/slides.js
@@ -1,14 +1,18 @@
 var observableModule = require("data/observable");
 var device = require("platform").device;
+var firebase = require("nativescript-plugin-firebase");
 var pageData = new observableModule.Observable();
+var frameModule = require("ui/frame");
+var topmost = frameModule.topmost;
 
 function pageLoaded(args) {
     var page = args.object;
     page.bindingContext = pageData;
+    firebase.init({});
 }
 
 exports.login = function() {
-    firebase.login({
+    /*firebase.login({
       type: firebase.LoginType.GOOGLE
     }).then(
       function (result) {
@@ -17,5 +21,6 @@ exports.login = function() {
       function (errorMessage) {
         console.log(errorMessage);
       }
-    );
+    );*/
+      topmost().navigate("home/home-page");
 }
